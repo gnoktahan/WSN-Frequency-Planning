ef593b243fb440c42313d8de746c89bdcc7522a4
gnoktahan <gnoktahan@gmail.com>
2016-08-17 13:41:22 +0200

    Structure has been changed because dataset contains discontinouity after each 5600 samples

--- KF_Advanced_Implementation_LLDN.m
+++ KF_Advanced_Implementation_LLDN.m
@@ -1,25 +1,50 @@
 % Read data from file:
+%{
 directory='/Users/GKN/Downloads/15.4_RSSI/';
 file_name='node1.txt';
 fileID=fopen(strcat(directory,file_name),'r');
 A=fscanf(fileID,'%f %f',[2000000 1]);
 fclose(fileID);
+%}
 
-s.x = [1,1,1,1,2,2,2,2,2,1]; %Initialization (first measurement results)
-s.A = 1;
-s.Q = 0.03^2; % variance, hence stdev^2
-s.H = 1;
-s.R = 0.06^2; % variance, hence stdev^2
-s.B = 0;
-s.u = 0;
-s.x = nan;
-s.P = nan;
+CISTER_RSSI_5600_500 = zeros(5600,500);
+for i = 1 : 500
+  CISTER_RSSI_5600_500(:,i) = CISTER_RSSI_250000(((i-1)*5600+1):(i*5600));
+end
 
-LLDN_FULL = [];
+accuracy = zeros(500,1);
 
-for k = 1 : 9195 % Until 330.000
+for scan = 1 : 500
+
+  clear s
+
   CISTER_RSSI = [];
-  CISTER_RSSI = CISTER_RSSI_250000(((k-1)*500+1):(k*500));
+  CISTER_RSSI = CISTER_RSSI_5600_500((1:500),scan);
+  RSSI_LLDN = [];
+  LLDN_SF = ones(1,10);
+  for i = 0 : 9 % 10 time slots
+    RSSI_LLDN = CISTER_RSSI(50*i+13 : 50*i+37);
+    if mean(RSSI_LLDN) < 14
+      LLDN_SF(i+1) = 2; % 2 means free 1 means occupied (interfered)
+    end
+  end
+
+  %s.x = [1,1,1,1,2,2,2,2,2,1]; %Initialization (first measurement results)
+  s.x = LLDN_SF; %Initialization (first measurement results)
+  s.A = 1;
+  s.Q = 0.03^2; % variance, hence stdev^2
+  s.H = 1;
+  s.R = 0.06^2; % variance, hence stdev^2
+  s.B = 0;
+  s.u = 0;
+  s.x = nan;
+  s.P = nan;
+
+  LLDN_FULL = [];
+
+for k = 1 : 11 % Until 5500
+  CISTER_RSSI = [];
+  CISTER_RSSI = CISTER_RSSI_5600_500((((k-1)*500+1):(k*500)),scan);
   RSSI_LLDN = [];
   RSSI_LLDN_FULL = [];
   LLDN_SF = ones(1,10);
@@ -44,6 +69,9 @@ end
 
 KF_out = round([s(2:end).x]);
 
+
+% plot it:
+%{
 figure
 hold on
 grid on
@@ -55,10 +83,14 @@ ht=plot(LLDN_FULL,'g-');
 legend([hz hk ht],'observations','Kalman output','true RSSI')
 title('Channel RSSI Estimation with Kalman Filter')
 hold off
+%}
+
 
 %LLDN_FULL_col = LLDN_FULL.'; %convert row to column
 %sqrt(immse([s(2:end).x],LLDN_FULL_col))
 
 % difference -> diversion -> take square -> take mean -> take square root -> finally multiply by 100:
 perc_err = 100*errperf(LLDN_FULL,KF_out,'rmsre'); %percentage root mean squared relative error
-accuracy = 100 - perc_err
+accuracy(scan,1) = 100 - perc_err;
+
+end
